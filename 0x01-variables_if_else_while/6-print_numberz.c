b6ddd32a79ad4768812168c347781bcf88c4e3e2
mnqobiMenzi <andilemlambo98@gmail.com>
2023-02-18 19:17:44 -0800

    6-print_numberz.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (complete)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
