a4dc1cd6d520d17a1ed7fad749186511d67c1070
andile <andilemlambo98@gmail.com>
2023-02-21 15:20:18 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Display natural numbers
+ *
+ * @n: natural number
+ *
+ * Return: natural number @n
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d,  ", i);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d. ", i);
+		}
+		printf("\n");
+
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("%d, ", n);
+	}
+}
