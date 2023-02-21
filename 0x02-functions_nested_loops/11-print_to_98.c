72b8d8e3472dea318fc90a91490d41006db54025
andile <andilemlambo98@gmail.com>
2023-02-21 15:46:19 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,9 +18,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-			{
 				printf(", ");
-			}
 		}
 		printf("\n");
 	}
@@ -29,28 +27,22 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			 if (i != 98)
-                        {
-                                printf(", ");
-                        }
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
-
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			 if (i != 98)
-                        {
-                                printf(", ");
-                        }
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-	{
 		printf("%d", n);
-	}
+
 }
