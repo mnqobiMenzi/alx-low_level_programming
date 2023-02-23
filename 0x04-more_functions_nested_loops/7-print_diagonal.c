21ce0f7d910978ce2b2e7a7847feec364c9f3fec
andile <andilemlambo98@gmail.com>
2023-02-23 15:11:11 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - diagonal lines
+ *
+ * @n: varaible
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		int y;
+
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				if (y == x)
+					_putchar('\\');
+				else if (y < x)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
