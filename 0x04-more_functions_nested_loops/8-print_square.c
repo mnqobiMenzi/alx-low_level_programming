5eef2f7132d037d63095cb64519de540987676b2
andile <andilemlambo98@gmail.com>
2023-02-23 15:29:41 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_square - Printing square
+*
+* @size: Square
+*/
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		int y;
+
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
