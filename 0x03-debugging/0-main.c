022ea069b58ee2a0253648b855e9cb9a530a49b1
andile <andilemlambo98@gmail.com>
2023-02-23 18:01:44 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Test Function
+ *
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	printf("Infinite loop incomong :(\n)");
+	x = 0;
+	/*while (x < 10 )*/
+	/*{*/
+	/*putchar(x);*/
+	/*}*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
