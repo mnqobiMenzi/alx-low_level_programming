97f6070d6d1fdc52da670f1044774a2e71690b15
andile <andilemlambo98@gmail.com>
2023-02-27 13:13:32 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 
 	longi = 0;
 
-	for (a = 0; str[s] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
 		longi++;
 
@@ -22,6 +22,6 @@ void puts_half(char *str)
 			n = ((longi + 1) / 2);
 		for (a = n; str[a] != '\0'; a++)
 			_putchar(str[a]);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
