9098e97cb33a15fd8d0078021665ef58378b2951
andile <andilemlambo98@gmail.com>
2023-02-27 14:05:12 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcpy - Function to copy
+ * @dest: copy to variable
+ * @src: copy from variable
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
